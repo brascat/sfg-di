499a7d20dc5797175e3756015ed44e9716273257
brascat <catarina.bras@diconium.com>
2022-05-11 10:07:25 +0100

    Add profiles

---
+++ src/main/java/guru/springframework/sfgdi/services/I18nPortugueseGreetingService.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile({"PT", "default"})
+@Service("18nService")
+public class I18nPortugueseGreetingService implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Olá!";
+    }
+}
